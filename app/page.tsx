415c924cd50ea92c6711989a395b3214bc0d4528
saidmaharramli <saidmaharramlidev@gmail.com>
2026-03-24 12:16:14 +0400

    finished introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,52 @@
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import pictureOfMe from '@/public/me.jpg'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div id="mainHomePage">
       <Navbar />
+
+      <div id="introduction-wrapper">
+
+        <div id="intro-text-wrapper">
+          <h1>Frontend Developer</h1>
+          <p>I’m Said Maharramli, a frontend developer turning ideas into interactive web experiences. I focus on clean code, smooth UI, and projects that actually work.</p>
+
+          <div id="hashtags-wrapper">
+            <div className="hashtag center">Frontend</div>
+            <div className="hashtag center">MinimalDesign</div>
+            <div className="hashtag center">Next.js</div>
+            <div className="hashtag center">Social Worker</div>
+          </div>
+
+          <div id="cta-projects-wrapper">
+            <Link id="cta-projects" href={'/projects'}>See my work</Link>
+          </div>
+
+        </div>
+
+        <div id="intro-card-wrapper">
+          <div id="photo-wrapper">
+            <Image id="pictureOfMe" src={pictureOfMe} alt="Picture" />
+
+
+          </div>
+          <span className="text-gray-300 pt-4 emailIntro" >saidmaharramlidev@gmail.com</span>
+
+          <Link href={'/about'} id="cta-intro-card" className="center">
+            About me
+
+          </Link>
+
+        </div>
+
+
+
+      </div>
+
+
 
     </div>
   );
